7b493772c590671db070a79af473ec58eeae413a
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2020-10-21 21:46:41 +0200

    Fixing guard test on create robot

--- lib/robot_hunter.ex
+++ lib/robot_hunter.ex
@@ -1,18 +1,19 @@
 defmodule RobotHunter do
-    defstruct(position: %{x: 0, y: 0}, orientation: :north)
+  defstruct(position: %{x: 0, y: 0}, orientation: :north)
 
-    @doc """
-    Create a new Robot
+  @doc """
+  Create a new Robot
 
-    ## Examples
+  ## Examples
 
-        iex> RobotHunter.createRobot()
-        %RobotHunter{position: %{x: 0, y: 0}, orientation: :north}  
+      iex> RobotHunter.createRobot()
+      %RobotHunter{position: %{x: 0, y: 0}, orientation: :north}  
 
-        iex> RobotHunter.createRobot(%{x: 1, y: -1}, :south)
-        %RobotHunter{position: %{x: 1, y: -1}, orientation: :south}             
+      iex> RobotHunter.createRobot(%{x: 1, y: -1}, :south)
+      %RobotHunter{position: %{x: 1, y: -1}, orientation: :south}             
 
-    """
-    def createRobot(pos \\ %{x: 0, y: 0}, orientation \\ :north) when orientation in [:north, :south, :west, :east], do: %RobotHunter{position: pos, orientation: orientation}
-
+  """
+  def createRobot(pos \\ %{x: 0, y: 0}, orientation \\ :north)
+      when orientation in [:north, :south, :west, :east],
+      do: %RobotHunter{position: pos, orientation: orientation}
 end
--- test/robot_hunter_test.exs
+++ test/robot_hunter_test.exs
@@ -2,16 +2,20 @@ defmodule RobotHunterTest do
   use ExUnit.Case
   doctest RobotHunter
 
-  
   test "New robot by default at x 0 y 0 and orientation towards north" do
-    assert RobotHunter.createRobot() == %RobotHunter{position: %{x: 0, y: 0}, orientation: :north}  
+    assert RobotHunter.createRobot() == %RobotHunter{position: %{x: 0, y: 0}, orientation: :north}
   end
 
   test "We're creating a robot in x 1 y -1 and orientation towards south" do
-    assert RobotHunter.createRobot(%{x: 1, y: -1}, :south) == %RobotHunter{position: %{x: 1, y: -1}, orientation: :south}  
+    assert RobotHunter.createRobot(%{x: 1, y: -1}, :south) == %RobotHunter{
+             position: %{x: 1, y: -1},
+             orientation: :south
+           }
   end
 
   test "We can't create a robot with a camembert orientation" do
-    assert_raise ArgumentError, RobotHunter.createRobot(%{x: 1, y: -1}, :camembert)
+    assert_raise FunctionClauseError, fn ->
+      RobotHunter.createRobot(%{x: 1, y: -1}, :camembert)
+    end
   end
 end
