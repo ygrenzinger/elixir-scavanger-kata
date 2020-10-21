4c89c73d7a526e9f4bef7782fd14090f1a0f8077
yannick grenzinger <yannick.grenzinger@gmail.com>
2020-10-21 19:27:42 +0200

    first session

---
+++ lib/robot_hunter.ex
@@ -0,0 +1,18 @@
+defmodule RobotHunter do
+    defstruct(position: %{x: 0, y: 0}, orientation: :north)
+
+    @doc """
+    Create a new Robot
+
+    ## Examples
+
+        iex> RobotHunter.createRobot()
+        %RobotHunter{position: %{x: 0, y: 0}, orientation: :north}  
+
+        iex> RobotHunter.createRobot(%{x: 1, y: -1}, :south)
+        %RobotHunter{position: %{x: 1, y: -1}, orientation: :south}             
+
+    """
+    def createRobot(pos \\ %{x: 0, y: 0}, orientation \\ :north) when orientation in [:north, :south, :west, :east], do: %RobotHunter{position: pos, orientation: orientation}
+
+end
---
+++ lib/treasure_hunter.ex
@@ -0,0 +1,15 @@
+defmodule TreasureHunter do
+  @moduledoc """
+  Documentation for `TreasureHunter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+  """
+  def hello do
+    :world
+  end
+end
