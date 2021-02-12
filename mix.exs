b752908d292647ff943e5720d5f25c73ba734504
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2021-02-12 19:42:45 +0100

    adding quixir dep

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule ScavangerKata.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      { :quixir, "~> 0.9", only: :test }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
